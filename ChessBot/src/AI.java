0f9a46e84465fce013cfa21efa27762e565f0ecf
SK <shrek2357@gmail.com>
2020-05-26 00:05:27 -0700

    deleted AllMoves class(extra), added javadocs for AI.java

--- ChessBot/src/AI.java
+++ ChessBot/src/AI.java
@@ -1,28 +1,66 @@
-import java.util.ArrayList;
 
+
+/**
+ *  The class that plays as the computer, finds the next best move by calculating a given amount of moves deep, the computer is always whtie
+ *
+ *  @author  Shreyas Kaasyap
+ *  @version May 25, 2020
+ *  @author  Period: 1
+ *  @author  Assignment: ChessBot
+ *
+ *  @author  Sources: Shreyas Kaasyap, Sidharth Kannan, Leo Yang
+ */
 public class AI {
+	/**
+	 * the current Board
+	 */
 	private Board board;
+	/**
+	 * the depth, the amount of moves in advance being calculated
+	 */
 	private int depth;
+	/**
+	 * a boolean that is true if the current position is checkmate
+	 */
 	private boolean checkmate = false;
+	
+	/**
+	 * a boolean that is true if the current position is checkmate and white has won the game
+	 */
 	private boolean whiteWon = false;
+	/**
+	 * @param b the current board
+	 * @param depth the amount of moves to calculate
+	 * A constructor for the AI class, instantiates fields
+	 */
 	public AI(Board b, int depth)
 	{
 		board = b;
 		this.depth = depth;
 	}
-	/**
-	 * 
-	 * @param color which player whose turn it is
-	 * @return the best possible board config after a given turn. returns null if no possible moves.
-	 */
 
+	/**
+	 * returns the checkmate boolean, true if position is checkmate, false otherwise   
+	 * @return boolean
+	 */
 	public boolean isCheckMate() {
 	    return checkmate;
 	}
 	
+	/**
+	 * returns the whiteWon boolean, true if white has won the game, false otherwise
+	 * @return boolean
+	 */
 	public boolean isWhite() {
 	    return whiteWon;
 	}
+	/**
+	 * Generates the next best move for the computer, recursive method that finds the min/max board based on the iteration, returns a Board that is the best possible Board for white calculating a certain number of moves in advance
+	 * @param board the current board
+	 * @param depth the amount of moves in advance left to calculate
+	 * @param color true if it is white's turn, false otherwise
+	 * @return Board 
+	 */
 	public Board minimax(Board board, int depth, boolean color) {
 		if(depth == 1)
 		{
